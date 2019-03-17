40ada2aed6d897538f14590acc081c77c6ebb3ab
zxn <zxn@qq.com>
2019-03-17 22:28:07 +0800

    3.17-22.27

--- src/views/main.vue
+++ src/views/main.vue
@@ -1,103 +1,137 @@
 <template>
-    <div>
-
-        <!-- <div
+  <div>
+    <!-- <div
             style="width: 100%;height: 25px;background-color: #184FBA;"
         >
 
-        </div> -->
-        
-        <router-view class="mainview">
-        </router-view>
+    </div>-->
+    <router-view class="mainview"></router-view>
 
-        <van-tabbar v-model="active" style="background-color: #F7F8F7;" active-color="#184FBA">
-                <van-tabbar-item icon="fire" to="guzhang" :replace=true :dot="true">首页</van-tabbar-item>
-                <van-tabbar-item icon="cart" to="xunjian" :replace=true >定位</van-tabbar-item>
-                <van-tabbar-item icon="friends" to="wode" :replace=true >我的</van-tabbar-item>
-        </van-tabbar>
-        
-    </div>
+    <van-tabbar v-model="active" style="background-color: #F7F8F7;" active-color="#184FBA">
+      <van-tabbar-item icon="fire" to="guzhang" :replace="true" :dot="true">首页</van-tabbar-item>
+      <van-tabbar-item icon="cart" to="xunjian" :replace="true">定位</van-tabbar-item>
+      <van-tabbar-item icon="friends" to="wode" :replace="true">我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
 </template>
 
 <script>
+import websocket from "./websocket";
 
-import websocket from './websocket'
-
-import dituService from '../services/ditu'
+import dituService from "../services/ditu";
 
 export default {
+  components: {
+    websocket
+  },
 
-    components: {
-        websocket
-    },
+  data() {
+    return {
+      active: 1,
+      title: "故障列表"
+    };
+  },
 
-    data (){
-        return {
-            active: 1,
-            title: "故障列表"
-        }
-    },
-
-    watch: {
-        active (newval, oldval) {
-            this.switchpage(newval)
-        }
-    },
-
-    mounted (){
-        setInterval(this.uploadposition, 2000)
-    },
-
-    methods: {
-
-        uploadposition(){
-            let _this = this
-            if ((typeof api) != undefined) {
-                var bMap = api.require('aMap');
-                bMap.getLocation( function(ret, err) {
-                    if (ret.status) {
-                        dituService.location({longitude: ret.lon,latitude: ret.lat}).then(res => {
-                            
-                        })
-                    } else {
-                        _this.$toast(err.code);
-                    }
-                });
-            }
-        },
-
-        switchpage(newval){
-            
-            switch (Number(newval)) {
-                case 0:
-                    this.$router.push("guzhang")
-                    
-                    break;
-                case 1:
-                    this.$router.push("xunjian")
-                    break;
-                case 2:
-                    this.$router.push("wode")
-                    break;
-            
-                default:
-
-                    break;
-            }
-        }
+  watch: {
+    active(newval, oldval) {
+      this.switchpage(newval);
     }
+  },
 
-}
+  mounted() {
+    // setInterval(this.uploadposition, 2000);
+  },
+
+  methods: {
+    uploadposition() {
+      let _this = this;
+      if (typeof api != undefined) {
+        var bMap = api.require("bMap");
+        bMap.getLocation(
+          {
+            accuracy: "100m",
+            autoStop: true,
+            filter: 1
+          },
+          function(ret, err) {
+            _this.$toast({
+              message: JSON.stringify(ret),
+              duration: 1000,
+              background: "#1989fa"
+            });
+            //   _this.$notify(JSON.stringify(ret));
+            //   _this.$notify({
+            //     message: JSON.stringify(ret),
+            //     duration: 1000,
+            //     background: "#1989fa"
+            //   });
+            if (ret.status) {
+              dituService
+                .location({ longitude: ret.lon, latitude: ret.lat })
+                .then(res => {});
+            } else {
+              _this.$toast(err.code);
+            }
+          }
+        );
+      }
+    },
+
+    uploadposition1() {
+      let _this = this;
+      if (typeof api != undefined) {
+        var bMap = api.require("aMap");
+        bMap.getLocation(function(ret, err) {
+          _this.$toast({
+            message: JSON.stringify(ret),
+            duration: 1000,
+            background: "#1989fa"
+          });
+          //   _this.$notify(JSON.stringify(ret));
+          //   _this.$notify({
+          //     message: JSON.stringify(ret),
+          //     duration: 1000,
+          //     background: "#1989fa"
+          //   });
+          if (ret.status) {
+            dituService
+              .location({ longitude: ret.lon, latitude: ret.lat })
+              .then(res => {});
+          } else {
+            _this.$toast(err.code);
+          }
+        });
+      }
+    },
+
+    switchpage(newval) {
+      switch (Number(newval)) {
+        case 0:
+          this.$router.push("guzhang");
+
+          break;
+        case 1:
+          this.$router.push("xunjian");
+          break;
+        case 2:
+          this.$router.push("wode");
+          break;
+
+        default:
+          break;
+      }
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-
-.mainview{
-    height: calc(100vh - 113px);
+.mainview {
+  height: calc(100vh - 113px);
 }
 
-.buttombar{
-    buttom: 0;
+.buttombar {
+  buttom: 0;
 }
 </style>
 
--- src/views/xunjian/index.vue
+++ src/views/xunjian/index.vue
@@ -1,210 +1,236 @@
 <template>
-    <div class="bodycontaner1">
-        <div>
-            <van-nav-bar title="定位" class="title-color">
-            </van-nav-bar>
-            <van-search
-                v-model="searchvalue"
-                placeholder="请输入搜索关键词"
-                show-action
-                shape="round"
-                @search="onSearch"
-                >
-                <div slot="action" @click="onSearch">搜索</div>
-            </van-search>
+  <div class="bodycontaner1">
+    <div>
+      <van-nav-bar title="定位" class="title-color"></van-nav-bar>
+      <van-search
+        v-model="searchvalue"
+        placeholder="请输入搜索关键词"
+        show-action
+        shape="round"
+        @search="onSearch"
+      >
+        <div slot="action" @click="onSearch">搜索</div>
+      </van-search>
 
-            <el-amap vid="amapDemo" class="zxndemo">
-                <el-amap-marker v-for="marker in locations" :position="marker.position">
-                </el-amap-marker>
-            </el-amap>
+      <el-amap vid="amapDemo" class="zxndemo">
+        <el-amap-marker v-for="marker in locations" :position="marker.position"></el-amap-marker>
+      </el-amap>
 
-            <bottom-bar></bottom-bar>
-
-            
-        </div>
-        
+      <bottom-bar></bottom-bar>
     </div>
+  </div>
 </template>
 
 <script>
+import { listobj, scan } from "../data";
 
-import { listobj, scan } from '../data'
-
-import dituService from '../../services/ditu'
-
+import dituService from "../../services/ditu";
 
 export default {
+  data() {
+    return {
+      scankeys: [
+        "categoryCode",
+        "categoryName",
+        "code",
+        "name",
+        "sectionCode",
+        "sectionName"
+      ],
+      locations: [],
+      searchvalue: ""
+    };
+  },
 
-    data(){
-        return {
-            scankeys: [
-                "categoryCode",
-                "categoryName",
-                "code",
-                "name",
-                "sectionCode",
-                "sectionName",
-            ],
-            locations: [],
-            searchvalue: ''
-        }  
+  mounted() {
+    this.getposition();
+  },
+
+  methods: {
+    getposition() {
+      let _this = this;
+
+      if (typeof api != undefined) {
+        var bMap = api.require("bMap");
+        bMap.getLocation(
+          {
+            accuracy: "100m",
+            autoStop: true,
+            filter: 1
+          },
+          function(ret, err) {
+            _this.$toast(JSON.stringify(ret));
+            if (ret.status) {
+              _this.locations.push({
+                position: [ret.lon, ret.lat],
+                name: "我的位置"
+              });
+            } else {
+              _this.$toast(err.code);
+            }
+          }
+        );
+      }
     },
 
-    mounted(){
-        this.getposition()
-    },
+    getposition1() {
+      let _this = this;
 
-    methods: {
-
-        getposition(){
-            let _this = this
-            if ((typeof api) != undefined) {
-                var bMap = api.require('aMap');
-                bMap.getLocation( function(ret, err) {
-                    if (ret.status) {
-                        _this.locations.push({position: [ret.lon, ret.lat], name : "我的位置"})
-                    } else {
-                        _this.$toast(err.code);
-                    }
-                });
-            }
-        },
-
-        onSearch(){
-            let _this = this
-            _this.locations = []
-            _this.getposition()
-            dituService.search({key: _this.searchvalue}).then(res => {
-                if (res.code == 2) {
-
-                    res.data.map(item => {
-                        _this.locations.push({position: [item.longitude, item.latitude], name: item.name})
-                    })
-
-                }
-            })
-        },
-
-        test(){
-            
-            // let timestamp = new Date().getTime()
-            // let newDate = new Date()
-            // newDate.setTime(timestamp)
-            // let res =  newDate.toLocaleString()
-
-            // console.log(res);
-
-// api.alert({
-//     title: 'testtitle',
-//     msg: 'testmsg',
-// }, function(ret, err) {
-
-// });
-
-// api.notification({
-//     sound:'default',
-//     notify: {
-//     title: '通知标题',
-//         content: '通知内容'
-//     }
-// });
-
-            let localconfig = localStorage.getItem("localconfig")
-
-            console.log(localconfig == null);
-            
-            
-
-        },
-
-        find(){
-            this.$router.push("add_record?type=shoudong")
-        },
-
-        openscan(){
-            let _this = this
-
-            if ((typeof api) == 'undefined'){
-                let info = '{"categoryCode":"COVIJCQ","categoryName":"CO/VI检测器","code":"000001YRKCOVI","name":"右洞入口段CO/VI检测器","sectionCode":"CU1X1Q5","sectionName":"实验室隧道一"}'
-                _this.$router.push(`add_record?info=${info}&type=saoma`)
-                return
-            }
-
-            var FNScanner = api.require('FNScanner');
-            FNScanner.open({
-                autorotation: true
-            }, function(ret, err) {
-                if (ret && ret.eventType == 'success') {
-                   
-                    if ( !_this.checkscanjson(ret.content) ) {
-                        _this.$toast("二维码内容错误")
-                        return
-                    }
-
-                    
-
-                    let info = ret.content
-                    // let info = '{"categoryCode": "COVIJCQ","categoryName": "CO/VI检测器","code": "001001YCKCOVI","name": "CO/VI检测器YCKCOVI","sectionCode": "CU4933Y","sectionName": "西簧隧道"}'
-                    _this.$router.push(`add_record?info=${info}&type=saoma`)
-                } else {
-                    if(err != ''){
-                        _this.$toast('扫码失败' + JSON.stringify(err))
-                    }
-                }
+      if (typeof api != undefined) {
+        var bMap = api.require("aMap");
+        bMap.getLocation(function(ret, err) {
+        //   _this.$toast(JSON.stringify(ret));
+          if (ret.status) {
+            _this.locations.push({
+              position: [ret.lon, ret.lat],
+              name: "我的位置"
             });
-        },
+          } else {
+            _this.$toast(err.code);
+          }
+        });
+      }
+    },
 
-        checkscanjson(content){
-            if ( ! this.$isJSON(content) ) {
-                return false
+    onSearch() {
+      let _this = this;
+      _this.$toast("2222");
+      _this.locations = [];
+      _this.getposition();
+      dituService
+        .search({ key: _this.searchvalue })
+        .then(res => {
+          _this.$toast(JSON.stringify(res));
+          if (res.code == 2) {
+            res.data.map(item => {
+              _this.locations.push({
+                position: [item.longitude, item.latitude],
+                name: item.name
+              });
+            });
+          }
+        })
+        .catch(res => {
+          _this.$toast(JSON.stringify(res));
+        });
+    },
+
+    test() {
+      // let timestamp = new Date().getTime()
+      // let newDate = new Date()
+      // newDate.setTime(timestamp)
+      // let res =  newDate.toLocaleString()
+
+      // console.log(res);
+
+      // api.alert({
+      //     title: 'testtitle',
+      //     msg: 'testmsg',
+      // }, function(ret, err) {
+
+      // });
+
+      // api.notification({
+      //     sound:'default',
+      //     notify: {
+      //     title: '通知标题',
+      //         content: '通知内容'
+      //     }
+      // });
+
+      let localconfig = localStorage.getItem("localconfig");
+
+      console.log(localconfig == null);
+    },
+
+    find() {
+      this.$router.push("add_record?type=shoudong");
+    },
+
+    openscan() {
+      let _this = this;
+
+      if (typeof api == "undefined") {
+        let info =
+          '{"categoryCode":"COVIJCQ","categoryName":"CO/VI检测器","code":"000001YRKCOVI","name":"右洞入口段CO/VI检测器","sectionCode":"CU1X1Q5","sectionName":"实验室隧道一"}';
+        _this.$router.push(`add_record?info=${info}&type=saoma`);
+        return;
+      }
+
+      var FNScanner = api.require("FNScanner");
+      FNScanner.open(
+        {
+          autorotation: true
+        },
+        function(ret, err) {
+          if (ret && ret.eventType == "success") {
+            if (!_this.checkscanjson(ret.content)) {
+              _this.$toast("二维码内容错误");
+              return;
             }
 
-            let con_json = JSON.parse(content)
-            let res = true
-            this.scankeys.map(item => {
-                if (con_json[item] == undefined) {
-                    res = false
-                }
-            })
-            return res
+            let info = ret.content;
+            // let info = '{"categoryCode": "COVIJCQ","categoryName": "CO/VI检测器","code": "001001YCKCOVI","name": "CO/VI检测器YCKCOVI","sectionCode": "CU4933Y","sectionName": "西簧隧道"}'
+            _this.$router.push(`add_record?info=${info}&type=saoma`);
+          } else {
+            if (err != "") {
+              _this.$toast("扫码失败" + JSON.stringify(err));
+            }
+          }
         }
+      );
+    },
+
+    checkscanjson(content) {
+      if (!this.$isJSON(content)) {
+        return false;
+      }
+
+      let con_json = JSON.parse(content);
+      let res = true;
+      this.scankeys.map(item => {
+        if (con_json[item] == undefined) {
+          res = false;
+        }
+      });
+      return res;
     }
-}
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-.xunjian_col{
-    text-align: center;
+.xunjian_col {
+  text-align: center;
 }
 
-.xunjiantabbar{
-    .van-nav-bar__title{
-        color: black;
-        margin-top: 2px;
-    }
+.xunjiantabbar {
+  .van-nav-bar__title {
+    color: black;
+    margin-top: 2px;
+  }
 }
 
-.xunjiancard{
-    margin: 5px;
-    background-color: white;
-    padding: 10px;
+.xunjiancard {
+  margin: 5px;
+  background-color: white;
+  padding: 10px;
 }
 
-.xunjiancardsd{
-    margin: 5px 0px 0px 0px;
-    background-color: white;
-    padding: 10px;
+.xunjiancardsd {
+  margin: 5px 0px 0px 0px;
+  background-color: white;
+  padding: 10px;
 }
 
-.xunjiancardsd1{
-    margin: 0px 0px 5px 0px;
-    background-color: white;
-    padding: 10px;
+.xunjiancardsd1 {
+  margin: 0px 0px 5px 0px;
+  background-color: white;
+  padding: 10px;
 }
 
 .zxndemo {
-    //   height: 100%;
-      height: calc(100vh - 146px);
+  //   height: 100%;
+  height: calc(100vh - 146px);
 }
-
 </style>
